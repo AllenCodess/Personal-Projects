31e7a784b6a55ff860539ae70e8ec0f6389b61cd
AllenCodess <Aph296@protonmail.com>
2026-01-16 18:28:19 -0500

    Recover lost files from previous commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let photoOne = document.getElementById('project1-photo')
+let buttonOne = document.querySelector('.button')
+
+buttonOne.addEventListener('click', function ButtonOneClick() {
+    photoOne.src = "https://imgs.search.brave.com/c3WFTfeMLtynRV8IKWkRHn1sUxQ79I0osiMmYZSD60c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTk1/MjM4MjE0Mi92ZWN0/b3IvY29taW5nLXNv/b24tYmFubmVyLXNp/Z24tYmFkZ2UtdmVj/dG9yLWRlc2lnbi5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/a0dGbURfY2ZCajQ2/QVVOX2FHVUNheFdN/eEFPR2U5VzNYVmY3/UldSVFBSQT0"
+})
+
+let photoTwo = document.getElementById('project2-photo')
+let buttonTwo = document.querySelector('.buttontwo')
+
+buttonTwo.addEventListener('click', function() {
+    photoTwo.src = "https://imgs.search.brave.com/c3WFTfeMLtynRV8IKWkRHn1sUxQ79I0osiMmYZSD60c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTk1/MjM4MjE0Mi92ZWN0/b3IvY29taW5nLXNv/b24tYmFubmVyLXNp/Z24tYmFkZ2UtdmVj/dG9yLWRlc2lnbi5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/a0dGbURfY2ZCajQ2/QVVOX2FHVUNheFdN/eEFPR2U5VzNYVmY3/UldSVFBSQT0"
+})
+
+let photoThree = document.getElementById('project3-photo')
+let buttonThree = document.querySelector('.buttonthree')
+
+buttonThree.addEventListener('click', function() {
+    photoThree.src = "https://imgs.search.brave.com/c3WFTfeMLtynRV8IKWkRHn1sUxQ79I0osiMmYZSD60c/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTk1/MjM4MjE0Mi92ZWN0/b3IvY29taW5nLXNv/b24tYmFubmVyLXNp/Z24tYmFkZ2UtdmVj/dG9yLWRlc2lnbi5q/cGc_cz02MTJ4NjEy/Jnc9MCZrPTIwJmM9/a0dGbURfY2ZCajQ2/QVVOX2FHVUNheFdN/eEFPR2U5VzNYVmY3/UldSVFBSQT0"
+})
